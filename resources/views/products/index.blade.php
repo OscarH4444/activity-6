bcad4e7dad192828e577c52b43c11c777deaabfd
Oscar <AL02925773@tecmilenio>
2025-03-01 21:11:18 -0600

    homework 5

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,1 +1,15 @@
-<title>Product View</title>
+<!-- resources/views/products/index.blade.php -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product View</title>
+</head>
+<body>
+    <h1>Lista de Productos</h1>
+
+    <a href="{{ route('products.create') }}">
+        <button>Add Product</button>
+    </a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
